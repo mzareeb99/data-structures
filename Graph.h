f9580ccb25375fa535c620ff8a8d405e9f2f4b27
obedat <67233375+mzareeb99@users.noreply.github.com>
2021-08-31 19:53:54 +0300

    Add files via upload

--- Graph.h
+++ Graph.h
@@ -18,7 +18,12 @@ class Proximity_matrix{
    int V;
    int E;
 public:
-    Proximity_matrix(int i): graph(mat(i)),V(i),E(0){}
+    Proximity_matrix(int i): graph(mat(i+1)),V(i+1),E(0){
+        for(int x = 1 ; x < i+1;x++){
+            graph[0][x]=x;
+            graph[x][0] = x;
+        }
+    }
     void insert() {}
     template <typename ...Args>
     void insert(const pair<int,int>& p, Args...more) {
@@ -29,7 +34,32 @@ public:
         }
         insert(more...);
     }
+
+    void remove() {}
+    template <typename ...Args>
+    void remove(const pair<int,int>& p, Args...more) {
+        if(graph[p.first][p.second] == 1) {
+            graph[p.first][p.second] = 0;
+            graph[p.second][p.first] = 0;
+            E--;
+        }
+        insert(more...);
+    }
     friend ostream& operator<<(ostream&,Proximity_matrix&);
+
+
+      vector<int> neighbors(int x){
+          vector<int> temp;
+          int y=0;
+          for(auto i : graph[x]){
+              if(i >0)
+                  temp.push_back(y);
+              y++;
+          }
+          temp.erase(temp.begin());
+
+            return temp;
+      }
 };
 ostream& operator<<(ostream& os ,Proximity_matrix& p){
     return os << p.graph<<endl;
@@ -45,8 +75,8 @@ public:
     template <typename ...Args>
     void insert(const pair<int,int>& p, Args...more) {
         bool temp = true;
-        for(auto  i : gra[p.first]){
-            if(i == p.second-1)
+        for(auto  i : gra[p.first-1]){
+            if(i == p.second)
                 temp = false;
         }
         if(temp) {
@@ -57,11 +87,25 @@ public:
         insert(more...);
     }
     friend ostream& operator<<(ostream&,Proximity_List&);
+
+
+    vector<int>& neighbors(int x){
+//        vector<int> temp;
+//        int y=0;
+//        for(auto i : gra[x]){
+//            if(i >0)
+//                temp.push_back(y);
+//            y++;
+//        }
+//        temp.erase(temp.begin());
+
+        return gra[x-1];
+    }
 };
 ostream& operator<<(ostream& os,Proximity_List& p){
     int j =0;
     for(auto & i : p.gra){
-        os<<++j<<endl;
+        os<<++j<<":"<<endl;
 
         if(i.size() != 0)
            os<< i<<endl;
